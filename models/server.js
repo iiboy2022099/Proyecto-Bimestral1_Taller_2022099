bbcfa54c1566e4750e92ffa365f9dffeaef928cd
iiboy-2022099 <iiboy-2022099@kinal.edu.gt>
2024-02-18 22:14:34 -0600

    subiendo user is the Admin, crud basic

--- models/server.js
+++ models/server.js
@@ -7,7 +7,7 @@ class Server{
     constructor(){
         this.app = express();
         this.port = process.env.PORT;
-       
+        this.usuariosPath = '/api/usuarios';
 
         this.conectarDB();
 
@@ -27,7 +27,7 @@ class Server{
     }
 
     routes(){
-       
+        this.app.use(this.usuariosPath, require('../routes/user.routes'));
        
     }
 
