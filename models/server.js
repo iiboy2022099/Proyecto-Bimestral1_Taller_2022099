0893d95401e0fe2e4fb771dcc70f2e9c0bb158d2
iiboy-2022099 <iiboy-2022099@kinal.edu.gt>
2024-02-18 19:59:55 -0600

    subiendo estructure

--- models/server.js
+++ models/server.js
@@ -7,16 +7,29 @@ class Server{
     constructor(){
         this.app = express();
         this.port = process.env.PORT;
-  
+       
 
         this.conectarDB();
 
+        this.middlewares();
+
+        this.routes();
     }
 
     async conectarDB(){
         await dbConnection();
     }
 
+    middlewares(){
+        this.app.use(express.static('public'));
+        this.app.use(cors());
+        this.app.use(express.json());
+    }
+
+    routes(){
+       
+       
+    }
 
 
     listen(){
